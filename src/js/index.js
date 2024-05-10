11b1fd7088f9e0779333128969e7f5857f307360
Kevin Ocampo <161964924+Ocampo81@users.noreply.github.com>
2024-05-10 16:30:07 +0000

    Definitivo sin hooks

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,20 @@
-//import react into the bundle
-import React from "react";
-import ReactDOM from "react-dom";
-import { Counter } from "./component/Counter";
-
-// include your styles into the webpack bundle
-import "../styles/index.css";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SecondsCounter from './component/SecondCounter';
+import '../styles/index.css'
 
 
-ReactDOM.render(<Counter />, document.getElementById('app'));
+// Define una función para iniciar el contador de segundos
+const startSecondsCounter = () => {
+  let seconds = 0;
+  setInterval(() => {
+    ReactDOM.render(
+      <SecondsCounter seconds={seconds} />,
+      document.getElementById('app')
+    );
+    seconds++;
+  }, 1000);
+};
+
+// Inicia el contador de segundos al cargar la página
+window.onload = startSecondsCounter;
