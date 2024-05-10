1031617e066b4f3586e4d99965c09c63a2288a95
Kevin Ocampo <161964924+Ocampo81@users.noreply.github.com>
2024-05-10 16:32:52 +0000

    Este si

--- src/js/index.js
+++ src/js/index.js
@@ -4,7 +4,7 @@ import SecondsCounter from './component/SecondCounter';
 import '../styles/index.css'
 
 
-// Define una función para iniciar el contador de segundos
+
 const startSecondsCounter = () => {
   let seconds = 0;
   setInterval(() => {
@@ -16,5 +16,4 @@ const startSecondsCounter = () => {
   }, 1000);
 };
 
-// Inicia el contador de segundos al cargar la página
 window.onload = startSecondsCounter;
